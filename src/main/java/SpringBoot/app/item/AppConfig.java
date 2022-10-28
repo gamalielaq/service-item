c294954c4ddd7c41cbdd092c63dfa26330cfdde9
Gamaliel <gabanto@canvia.com>
2022-10-27 22:40:13 -0500

    Integración de tolerancia de fallos circuit breaker con resilience4j

--- src/main/java/SpringBoot/app/item/AppConfig.java
+++ src/main/java/SpringBoot/app/item/AppConfig.java
@@ -1,9 +1,18 @@
 package SpringBoot.app.item;
 
+
+import java.time.Duration;
+
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 
 @Configuration
 public class AppConfig {
@@ -12,6 +21,23 @@ public class AppConfig {
     @LoadBalanced //Uliza por debajo ribbon para el baleanceo de carga
     public RestTemplate resgisterRestTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {  //Configuración Circuit Breaker
+        return factory -> factory.configureDefault(id -> {
+            return new Resilience4JConfigBuilder(id)
+                    .circuitBreakerConfig(CircuitBreakerConfig.custom()
+                        .slidingWindowSize(10)
+                        .failureRateThreshold(50)
+                        .waitDurationInOpenState(Duration.ofSeconds(10L))
+                        .permittedNumberOfCallsInHalfOpenState(5) //llamadas permitidas en el estado semiabierto
+                        .slowCallRateThreshold(50)
+						.slowCallDurationThreshold(Duration.ofSeconds(2L))
+                        .build())
+                    .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(3L)).build()) // Llamada lenta 3 segundos
+                    .build();
+        });
     }
     
 }
--- src/main/java/SpringBoot/app/item/controllers/ItemController.java
+++ src/main/java/SpringBoot/app/item/controllers/ItemController.java
@@ -1,6 +1,7 @@
 package SpringBoot.app.item.controllers;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 import SpringBoot.app.item.models.Item;
 import SpringBoot.app.item.models.Producto;
 import SpringBoot.app.item.services.IItemService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 @RestController
 @RequestMapping("/api")
@@ -38,12 +41,30 @@ public class ItemController {
         return this.itemService.findAll();
     }
 
-
     // @HystrixCommand(fallbackMethod = "metodoAlternativo") // Manejamos tolerancia de fallos
     @GetMapping("/listar/{id}/cantidad/{cantidad}")
     public Item detail(@PathVariable Long id, @PathVariable Integer cantidad) {
-        return this.cbFactory.create("items").run(() ->  this.itemService.findById(id, cantidad),e -> metodoAlternativo(id, cantidad, e));
+        // creamos un corto circuito llamado items
+        return this.cbFactory.create("items") 
+                    .run(() ->  this.itemService.findById(id, cantidad),e -> metodoAlternativo(id, cantidad, e));
     }
+
+    
+    @CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo")
+    @GetMapping("/listar/v1/{id}/cantidad/{cantidad}")
+    public Item detail2(@PathVariable Long id, @PathVariable Integer cantidad) {
+        return this.itemService.findById(id, cantidad);
+                    
+    }
+
+    @CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo2")
+    @TimeLimiter(name = "items")
+    @GetMapping("/listar/v2/{id}/cantidad/{cantidad}")
+    public CompletableFuture<Item> detail3(@PathVariable Long id, @PathVariable Integer cantidad) {
+        // CompletableFuture envolvemos en una llamada futura asincrona
+        return CompletableFuture.supplyAsync( () -> this.itemService.findById(id, cantidad) );               
+    }
+
 
     @GetMapping("/test")
     public String test() {
@@ -52,7 +73,7 @@ public class ItemController {
     
     public Item metodoAlternativo(Long id, Integer cantidad, Throwable e) {
         
-        log.info(e.getMessage());
+        log.info("Mensage: " + e.getMessage());
         
         Item item = new Item();
         Producto producto= new Producto();
@@ -65,6 +86,21 @@ public class ItemController {
         return item;
     }
     
+
+    public CompletableFuture<Item> metodoAlternativo2(Long id, Integer cantidad, Throwable e) {
+        
+        log.info("Mensage: " + e.getMessage());
+        
+        Item item = new Item();
+        Producto producto= new Producto();
+        
+        item.setCantidad(cantidad);
+        producto.setId(id);
+        producto.setNombre("Camara Sony");
+        producto.setPrecio(500.00);
+        item.setProducto(producto);
+        return CompletableFuture.supplyAsync(() -> item);
+    }
     
     
 }
